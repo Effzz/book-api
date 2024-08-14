90555c55d0f45529d4644a1ec712e017f1df8945
Efendi Salim <efendy.s.89@gmail.com>
2024-08-15 01:23:00 +0700

    add rate limiter

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import rateLimit from 'express-rate-limit';
+
 import BookRoutes from './routes/Book.routes';
 
 dotenv.config();
@@ -7,8 +9,15 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+// Rate limiter configuration
+const apiLimiter = rateLimit({
+    windowMs: 60 * 60 * 1000, // 1 hour
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: 'Too many requests from this IP, please try again later.'
+});
+
 // Append routes
-app.use('/api', BookRoutes);
+app.use('/api', apiLimiter, BookRoutes);
 
 const port = process.env.PORT || 3000;
 
